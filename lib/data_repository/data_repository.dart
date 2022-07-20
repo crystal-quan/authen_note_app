a3805a02fa880f779f63aa7ec96bf8ec3af7e83d
LingZhang99 <crystalliu25081987@gmail.com>
2022-07-20 09:56:07 +0700

    fix UI reposive

--- lib/data_repository/data_repository.dart
+++ lib/data_repository/data_repository.dart
@@ -1,3 +1,4 @@
+import 'package:authen_note_app/modules/note_modules.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -7,27 +8,40 @@ final currentUser = FirebaseAuth.instance.currentUser;
 
 final db = FirebaseFirestore.instance;
 
-class DataRepository {
+class NoteRepository {
+  NoteRepository({
+    this.note,
+  });
+  Note? note;
+
   void addData() async {
     bool result = await InternetConnectionChecker().hasConnection;
     if (result == true) {
       print('YAY! Free cute dog pics!');
       if (currentUser?.uid != null) {
+        Map<String, dynamic> data = {
+          'title': note?.title,
+          'content': note?.content,
+          'timeCreate': note?.timeCreate,
+        };
         final a = await db
             .collection("users")
             .doc("${currentUser!.email}")
             .collection('notes')
-            .add(note);
+            .add(data);
 
-        await db
+        db
             .collection("users")
-            .doc("${currentUser.email}")
+            .doc("${currentUser!.email}")
             .collection('notes')
             .doc(a.id)
             .set({'note id': a.id}, SetOptions(merge: true));
+      }else{
+        
       }
     } else {
       print('No internet :( Reason:');
+      
     }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:authen_note_app/app/app.dart';
 import 'package:authen_note_app/firebase_options.dart';
+import 'package:authen_note_app/google_login_page/google_login_screen.dart';
+import 'package:authen_note_app/home/view/home_page.dart';
 import 'package:authentication_repository/authentication_repository.dart';
 import 'package:bloc/bloc.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-
 
 Future<void> main() {
   return BlocOverrides.runZoned(
